668fa4ed46ae4473a8b3731b23d2fdb8d97aa197
Lachlan Graham <grala465@student.otago.ac.nz>
2023-07-25 13:50:20 +1200

    Working on the birdcards for Assignment 1

--- Assignment1/main.js
+++ Assignment1/main.js
@@ -1,10 +1,49 @@
-async function fetchData() {
-	const response = await fetch("./data/nzbird.json");
-	if (!response.ok){
-		console.error(response.status); // error handling
-	}
-	const data = response.json()  // parse to JSON
-	console.log(data); // use the data
-}
+// let birb_data;
 
-fetchData();
+// async function fetchData() {
+// 	const response = await fetch('nzbird.json');
+// 	if (!response.ok){
+// 		console.error(response.status); // error handling
+// 	}
+// 	const data = await response.json();  // parse to JSON
+// 	birb_data = data;
+// }
+
+// function createBirbCard(b) {
+// 	let birb_card = document.createElement("article");
+// 	birb_card.setAttribute("class", "birbcard");
+
+// 	let image = document.createElement("img");
+// 	image.setAttribute("src", b["photo"]["source"]);
+
+// 	let maori_name = document.createElement("h3");
+// 	maori_name.textContent = b["primary_name"];
+
+// 	let english_name = document.createElement("h2");
+// 	english_name.textContent = b["english_name"];
+
+// 	let description = document.createElement("p");
+// 	description.textContent = b["description"];
+
+// 	birb_card.appendChild(image);
+// 	birb_card.appendChild(maori_name);
+// 	birb_card.appendChild(english_name);
+// 	birb_card.appendChild(description);
+
+// 	let main = document.querySelector("#main");
+// 	main.appendChild(birb_card);
+// }
+
+// function createBirbCards() {
+// 	for (b in birb_data) {
+// 		createBirbCard(birb_data[b]);
+// 	}
+// }
+
+// fetchData();  // call the async function to populate birb_data
+// createBirbCards();
+// createBirbCards();
+
+
+
+
